468243d79dc5df35e856542620a6edc3c0e98048
Julio-Baeta1 <julbay1643@gmail.com>
2015-09-05 14:34:37 +0200

    ff

---
+++ sfmlg/sfmlg/sfmlg.cpp
@@ -0,0 +1,12 @@
+// sfmlg.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << 1;
+	return 0;
+}
+
